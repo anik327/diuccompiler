fcd288c2b17d0dc212f0495edd37b44ea5033049
Anik Das <hello@anikdas.dev>
2023-05-12 22:25:12 +0600

    Added token structure

--- compiler.h
+++ compiler.h
@@ -2,6 +2,50 @@
 #define DIUCCOMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+
+struct position
+{
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+
+    // true if their is whitespace between the token and the next token
+    bool whitespace;
+
+    //(61+19)
+    const char* between_brackets;
+
+};
 
 enum
 {
@@ -25,6 +69,6 @@ struct compile_process
 };
 
 int compile_file(const char* filename, const char* out_filename, int flags);
-struct compile_process *compile_process_create(const char *filename, const char *filename_out, int flags);
+struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags);
 
 #endif
